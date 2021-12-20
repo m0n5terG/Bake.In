3c4493bb4b3852ef62dbc6cf60a85f7209125fc8
m0n5terG <mikotai@yahoo.com>
2021-12-20 16:25:05 +0800

    fix public function

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -15,7 +15,7 @@ class PostsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['except' => 'index', 'show']);
+        // $this->middleware('auth', ['except' => 'index', '/blog.show']);
     }
     /**
      * Display a listing of the resource.
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 <div class="container">
-    
     <hr class="featurette-divider">
+
+    @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
     <div class="d-grid d-md-block ms-md-3 gap-2 mb-2">
         <form action="{{ route('blog.destroy',$post->id) }}" method="POST"> 
             <a href="/blog/{{ $post->id }}/edit" class="btn btn-secondary btn-sm">Edit</a>
@@ -11,9 +12,10 @@
             @csrf 
             @method('DELETE')
             <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+        </form>
     </div>
-    
-    <div class="row featurette">
+    @endif
+    <div class="row featurette mb-5">
         <div class="col-sm-12 col-md-6 order-md-2">
             <h2 class="featurette-heading">
                 {{ $post->title }}
@@ -29,6 +31,31 @@
             <img src="{{ asset('images/' . $post->image) }}" class="rounded mw-100">
         </div>
     </div>
+
+    {{-- <div class="row">
+        <div id="comment-form" class="col-md-8 offset-2">
+            <form action="{{ route('comment.store')}}" method="POST">
+            @csrf
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="" class="form-label">Name</label>
+                        <input type="name" class="form-control" name="" id="" placeholder="">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="" id="" placeholder="">
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="" class="form-label">Comment</label>
+                        <textarea class="form-control" name="comment" id="" rows="3"></textarea>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div> --}}
 </div>   
 
 @endsection
