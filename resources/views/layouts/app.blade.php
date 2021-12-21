15de0bebd1e08bfe9848d0015228fdfb04edf6da
mon5terG <mikotai@yahoo.com>
2021-12-21 09:53:23 +0800

    new branch commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}" hidden>{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
